5b13fdd09ba8b2cebcbb856e612e2bc3be7e3ee9
GregorySian <metalcrusader96@gmail.com>
2019-08-26 21:33:48 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,59 +1,17 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 
-namespace Thema_Simeio
+namespace Thema_2019_Sphere
 {
-    class Simeio
-    {
-        public int _x;
-        public int _y;
-
-        public Simeio()
-        {
-
-        }
-        public Simeio(int x, int y)
-        {
-            _x = x;
-            _y = y;
-        }
-
-        public override string ToString()
-        {
-            return $"x = {_x} y = {_y}" ;
-        }
-    }
-
-    class Kyklos : Simeio
-    {
-        private double r;
-        private static double pi = 3.14;
-
-
-        public Kyklos(int x, int y, double radius): base(x,y)
-        {
-            r = radius;
-        }
-        public double Parameter()
-        {
-            return 2 * pi * r;
-
-        }
-        public override string ToString()
-        {
-            return $"x = {_x} y = {_y} Parameter = {Parameter()}" ;
-        }
-    }
-    
-
     class Program
     {
         static void Main(string[] args)
         {
-            Simeio s1 = new Simeio(150,200);
-            Kyklos k1 = new Kyklos(50,100,14.2);
-            Console.WriteLine(s1);
-            Console.WriteLine(k1);
+            Sphere s1 = new Sphere(5.0,6.0,7.0,3.0);
+            Sphere s2 = new Sphere(5);
+            Sphere s3 = new Sphere();
+
+            Console.WriteLine(s1.ToString() + "\n" + s2.ToString() + "\n" + s3.ToString() + "\n" );
+
         }
     }
 }
---
+++ Sphere.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thema_2019_Sphere
+{
+    class Sphere
+    {
+        private double _x;
+        private double _y;
+        private double _z;
+        private double _r;
+        private const double pi = 3.14;
+
+
+        public double Embadon()
+        {
+            return 4.0/3.0 * pi * Math.Pow(_r,3);
+        }
+
+        public Sphere(double x, double y, double z, double r)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+            _r = r;
+        }
+
+        public Sphere(double r)
+        {
+            _r = r;
+        }
+
+        public Sphere() : this(0.0, 0.0, 0.0, 1.0)
+        {
+
+        }
+
+        public override String ToString()
+        {
+            return $"({_x},{_y},{_z},{_r}) = {Embadon()}";
+        }
+    }
+}
